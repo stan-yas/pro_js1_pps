3e3993315600f338eb4d389bf819728596092d8f
Lukas <stanislav.yas@ya.ru>
2020-02-09 20:59:43 +0600

    kekstagram: module3-task1 completed

--- kekstagram/js/pictures.js
+++ kekstagram/js/pictures.js
@@ -8,6 +8,8 @@ var COMMENTS = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
+var socialNames = ['facebook', 'twitter', 'vk', 'instagram'/* , 'youtube', 'ok'*/];
+
 /**
  * Getting a random integer between two values
  * @param {number} min The minimum is inclusive
@@ -39,9 +41,59 @@ function Picture(i) {
   })();
 }
 
-var pictures = [];
-for (var i = 0; i < 5; i++) {
-  pictures.push(new Picture(i + 1));
+function generatePictures(pictureObjects, pictureTemplate) {
+  var documentFragment = document.createDocumentFragment();
+  for (var i = 0; i < pictureObjects.length; i++) {
+    var pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('img').src = pictureObjects[i].url;
+    pictureElement.querySelector('.picture-likes').textContent = pictureObjects[i].likes.toString();
+    pictureElement.querySelector('.picture-comments').textContent = pictureObjects[i].comments.length.toString();
+    documentFragment.appendChild(pictureElement);
+  }
+  return documentFragment;
 }
 
-console.log(pictures);
+function renderComments(commentsContainer, comments) {
+  if (!commentsContainer || comments.length === 0) {
+    return;
+  }
+  // var commentsFragment = document.createDocumentFragment();
+  var ul = document.createElement('ul');
+  ul.className = 'social__comments';
+  for (var i = 0; i < comments.length; i++) {
+    var li = document.createElement('li');
+    li.className = 'social-comments__comment';
+    var icon = document.createElement('div');
+    icon.className = 'social-comments-icon social-icon-' + getRandomArrayValue(socialNames);
+    // var img = document.createElement('img');
+    // // img.className = '';
+    // img.src = 'img/' + getRandomArrayValue(socialNames) + '.svg';
+    // img.alt = 'Аватар комментатора фотографии';
+    // img.width = 35;
+    // img.height = 35;
+    var p = document.createElement('p');
+    p.textContent = comments[i];
+    li.appendChild(icon);
+    li.appendChild(p);
+    ul.appendChild(li);
+    // commentsFragment.appendChild(li);
+  }
+  commentsContainer.appendChild(ul);
+  // commentsContainer.appendChild(commentsFragment);
+}
+var pictures = [];
+for (var i = 0; i < 25; i++) {
+  pictures.push(new Picture(i + 1));
+}
+var pictureTemplate = document.querySelector('#picture-template').content;
+var pictureElements = generatePictures(pictures, pictureTemplate); // create pictures
+document.querySelector('.pictures').appendChild(pictureElements); // render pictures
+
+var galleryOverlay = document.querySelector('.gallery-overlay');
+galleryOverlay.classList.remove('hidden'); // show galleryOverlay
+galleryOverlay.querySelector('.gallery-overlay-image').src = pictures[0].url;
+galleryOverlay.querySelector('.likes-count').textContent = pictures[0].likes.toString();
+galleryOverlay.querySelector('.comments-count').textContent = 'комментариев: ' + pictures[0].comments.length.toString();
+renderComments(galleryOverlay.querySelector('.gallery-overlay-controls-comments'), pictures[0].comments);
+document.querySelector('.comments-count').classList.add('hidden');
+// console.log(pictures);
