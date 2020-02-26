b8344c52884982796c9697bb504964e1e799c9ee
Lukas <stanislav.yas@ya.ru>
2020-02-26 23:08:42 +0600

    dragging dialog added

--- wizards/js/dialog.js
+++ wizards/js/dialog.js
@@ -5,19 +5,38 @@
   window.dialog = {
     element: document.querySelector('.setup'),
     open: function () {
+      dialog.element.style.left = '50%';
+      dialog.element.style.top = '80px';
       dialog.element.classList.remove('hidden');
+      openElement.removeEventListener('click', dialog.open);
+      openElement.removeEventListener('keydown', function (evt) {
+        if (evt.key === 'Enter'/* || evt.key === 'Space'*/) {
+          dialog.open();
+        }
+      });
       document.addEventListener('keydown', onDocumentKeyDown, true);
+      document.addEventListener('mousedown', onDocumentMouseDown, true);
     },
     close: function () {
       dialog.element.classList.add('hidden');
+      openElement.addEventListener('click', dialog.open);
+      openElement.addEventListener('keydown', function (evt) {
+        if (evt.key === 'Enter') {
+          dialog.open();
+        }
+      });
       document.removeEventListener('keydown', onDocumentKeyDown, true);
-    }
+      document.removeEventListener('mousedown', onDocumentMouseDown, true);
+    },
+    dragging: false
   };
 
   var openElement = document.querySelector('.setup-open');
   var closeElement = dialog.element.querySelector('.setup-close');
   var formElement = dialog.element.querySelector('form.setup-wizard-form');
-  var userPicElement = formElement.querySelector('.setup-user-pic');
+  var userPicElement = formElement.querySelector('div.upload input');
+
+  dialog.close(); // init open dialog listeners
 
   // render similar wizards
   var documentFragment = document.createDocumentFragment();
@@ -28,22 +47,12 @@
   dialog.element.querySelector('.setup-similar').classList.remove('hidden');
 
   // adding event listeners
-  openElement.addEventListener('click', dialog.open);
-
-  openElement.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Enter'/* || evt.key === 'Space'*/) {
-      dialog.open();
-    }
-  });
-
   closeElement.addEventListener('click', dialog.close);
-
   closeElement.addEventListener('keydown', function (evt) {
     if (evt.key === 'Enter') {
       dialog.close();
     }
   });
-
   formElement.addEventListener('submit', function (evt) {
     evt.currentTarget.action = 'https://js.dump.academy/code-and-magick';
   });
@@ -77,4 +86,42 @@
       dialog.close();
     }
   }
+
+  function onDocumentMouseDown(evt) {
+    if (evt.target === userPicElement) {
+      mouse.x = evt.clientX;
+      mouse.y = evt.clientY;
+      document.addEventListener('mousemove', onDocumentMouseMove, true);
+      document.addEventListener('mouseup', onDocumentMouseUp, true);
+      evt.stopPropagation();
+    }
+  }
+
+  var mouse = {
+    x: undefined,
+    y: undefined
+  };
+
+  var onDocumentMouseMove = function (evt) {
+    dialog.dragging = true;
+    dialog.element.style.left = dialog.element.offsetLeft + (evt.clientX - mouse.x) + 'px';
+    dialog.element.style.top = dialog.element.offsetTop + (evt.clientY - mouse.y) + 'px';
+    mouse.x = evt.clientX; mouse.y = evt.clientY;
+    evt.stopPropagation();
+  };
+
+  var onDocumentMouseUp = function (evt) {
+    document.removeEventListener('mousemove', onDocumentMouseMove, true);
+    document.removeEventListener('mouseup', onDocumentMouseUp, true);
+    if (dialog.dragging) {
+      var fileInputHandler = function (ev) {
+        ev.preventDefault(); // prevent default if dialog dragged before 'mouseup' event
+        userPicElement.removeEventListener('click', fileInputHandler);
+      };
+      userPicElement.addEventListener('click', fileInputHandler);
+      dialog.dragging = false;
+    }
+    evt.stopPropagation();
+  };
+
 })();
